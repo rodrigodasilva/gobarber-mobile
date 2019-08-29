033ee956a9558bda6f46df727d506d3c90c5d862
rodrigodasilva <ribeiro.rodrigodasilva>
2019-08-29 16:52:24 -0300

    estilizando pagina signup

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -15,7 +15,7 @@ import {
   SignLinkText,
 } from './styles';
 
-export default function SignIn() {
+export default function SignIn({ navigation }) {
   return (
     <Background>
       <Container>
@@ -39,7 +39,7 @@ export default function SignIn() {
           <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
         </Form>
 
-        <SignLink onPress={() => {}}>
+        <SignLink onPress={() => navigation.navigate('SignUp')}>
           <SignLinkText>Criar conta gratuita</SignLinkText>
         </SignLink>
       </Container>
